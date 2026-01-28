68fe05a08c4400290b3a689301b65f2e88c8ac10
vallabh sathe <vallabhsathe9906@gmail.com>
2026-01-28 17:14:05 +0530

    backend cookies

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -353,7 +353,7 @@ export const githubCallback = async (req, res) => {
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
